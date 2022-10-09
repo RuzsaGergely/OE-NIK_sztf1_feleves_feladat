06ebdb13aeb8dd3337647532ba55c2d982568705
Ruzsa Gergely Gábor <ruzsa.gergely@ruzger.hu>
2022-10-09 22:22:10 +0200

    AwariGame class

---
+++ Awari/Awari/AwariGame.cs
@@ -0,0 +1,96 @@
+﻿/* OE-NIK # Ruzsa Gergely Gábor # BR1GHH
+ * SzTF1 - Féléves projekt - Maják kincse (Awari)
+ * Váltózok nyelve: angol / camelCase
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awari
+{
+    internal class AwariGame
+    {
+        // Pits of the user
+        public int[] pitsOfA;
+        // Pits of the computer
+        public int[] pitsOfB;
+
+        // Points of the user
+        public int pointsOfA = 0;
+        // Points of the computer
+        public int pointsOfB = 0;
+
+        // 0 - user ; 1 - computer
+        private int turn = 0;
+
+        public AwariGame()
+        {
+            pitsOfA = new int[] { 4,4,4,4,4,4 };
+            pitsOfB = new int[] { 4,4,4,4,4,4 };
+        }
+
+        public void drawGameplay()
+        {
+            Console.Clear();
+            // Draw computer's pits
+            Console.Write($"[{pointsOfB}] ");
+            for (int i = 0; i < pitsOfB.Length; i++)
+            {
+                Console.Write($"[{pitsOfB[i]}] ");
+            }
+            Console.Write($"A\n");
+            Console.Write($" B ");
+            for (int i = 0; i < pitsOfA.Length; i++)
+            {
+                Console.Write($"[{pitsOfA[i]}] ");
+            }
+            Console.Write($"[{pointsOfA}]\n\n");
+        }
+
+        public void askUserMove()
+        {
+            int input = 0;
+            do
+            {
+                Console.Write("Lépés?: ");
+                int.TryParse(Console.ReadLine(), out input);
+            } while (pitsOfA[input] == 0);
+            moveSeeds(turn, input);
+        }
+
+        private void moveSeeds(int player, int pit)
+        {
+            int leftover = pitsOfA[pit];
+            pitsOfA[pit] = 0;
+            for (int i = pit+1; i < pitsOfA.Length && leftover > 0; i++)
+            {
+                pitsOfA[i]++;
+                leftover--;
+            }
+            while (leftover > 0)
+            {
+                for (int i = 0; i < pitsOfB.Length && leftover > 0; i++)
+                {
+                    pitsOfB[i]++;
+                    leftover--;
+                }
+                for (int i = 0; i < pitsOfA.Length && leftover > 0; i++)
+                {
+                    pitsOfA[i]++;
+                    leftover--;
+                }
+            }
+            drawGameplay();
+        }
+
+
+        private void checkIfPlayerGetsPoint()
+        {
+
+        }
+
+        
+    }
+}
---
+++ Awari/Awari/Program.cs
@@ -0,0 +1,27 @@
+﻿/* OE-NIK # Ruzsa Gergely Gábor # BR1GHH
+ * SzTF1 - Féléves projekt - Maják kincse (Awari)
+ * Váltózok nyelve: angol / camelCase
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AwariGame game = new AwariGame();
+            game.drawGameplay();
+            while (true)
+            {
+                game.askUserMove();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
